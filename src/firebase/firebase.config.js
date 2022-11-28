6577bea19da5ac30ed817c00071af80ea40880b1
JubayerDev <ajashes858@gmail.com>
2022-11-29 01:06:41 +0600

    red-onion sart

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA2AUhg_hsjur-NUwhSclrX8S4dVyGvRLk",
+  authDomain: "red-onion-react-e06d2.firebaseapp.com",
+  projectId: "red-onion-react-e06d2",
+  storageBucket: "red-onion-react-e06d2.appspot.com",
+  messagingSenderId: "496011469983",
+  appId: "1:496011469983:web:a189eb76d4b09634c7aeaa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
